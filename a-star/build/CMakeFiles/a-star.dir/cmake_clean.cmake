3fc53276b81006a8f6add46522f7eb8aeb44231e
Unknown <luismarzor@gmail.com>
2018-09-17 10:34:26 +0200

    a-star

---
+++ a-star/build/CMakeFiles/a-star.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a-star.dir/source/AStar.cpp.o"
+  "liba-star.pdb"
+  "liba-star.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a-star.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ a-star/build/CMakeFiles/a_star_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_star_c.dir/source/a_star_c.cpp.o"
+  "a_star_c.pdb"
+  "a_star_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_star_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
